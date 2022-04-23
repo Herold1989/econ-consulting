c8afc7e3743dceec06778ce57bdfac631862236c
herold1989 <jens_herold@gmx.net>
2022-04-23 23:06:57 +0200

    added my self-designed banner logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
-import logo from './images/logo.png'
+import logo from './images/logo_HEFC.png'
 
 import './Navbar.css'
 
