bfc00ea79216bdbe96b84c2064ca7d30bae351d8
herold1989 <jens_herold@gmx.net>
2022-04-25 15:57:49 +0200

    added right hyperlink to logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className = 'header'> 
         <nav className='navbar'>
-            <a href='/' className = 'logo'>
+            <a href='#home' className = 'logo'>
                 <img src={logo} alt='logo' />
                 </a>
                 <div className = 'hamburger' onClick={handleClick}>
